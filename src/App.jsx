6d5933bbdb6f76c6c96bd54248e570317c81e43f
yegeto <115154804+yegeto@users.noreply.github.com>
2025-04-17 20:43:06 +0300

    enable components to show random numbers -A function was created to generate an array of random numbers between 1 and 6. -This array is stored in a state variable using the useState hook. -The state is then mapped over to render individual components for each number.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
+import { useState } from "react";
+
 import Die from "./components/Die";
 
 function App() {
+  const [dice, setDice] = useState(generateAllNewDice(10));
+
+  function generateAllNewDice(diceAmount) {
+    const newDiceArray = [];
+    for (let i = 0; i < diceAmount; i++) {
+      const roll = Math.floor(Math.random() * 6) + 1;
+      newDiceArray.push(roll);
+    }
+    return newDiceArray;
+  }
+
+  const diceElements = dice.map((number, index) => (
+    <Die key={index} value={number} />
+  ));
+
   return (
     <main>
-      <section className="dice-container">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-      </section>
+      <section className="dice-container">{diceElements}</section>
     </main>
   );
 }
