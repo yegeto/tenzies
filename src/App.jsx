a7703282d23ac92fbe1edc194a77689ef7ce7347
yegeto <115154804+yegeto@users.noreply.github.com>
2025-04-17 20:05:43 +0300

    create dice component that shows up on the page manually create 10 instances. use props to transfer data to the child

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import Die from "./components/Die";
+
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <section className="dice-container">
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Die.jsx
@@ -0,0 +1,5 @@
+import "./Die.css";
+
+export default function Die(props) {
+  return <button>{props.value}</button>;
+}
