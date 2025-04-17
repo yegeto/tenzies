a858fcb3534c59cfe13a5a6afb74212043b7c8df
yegeto <115154804+yegeto@users.noreply.github.com>
2025-04-17 23:21:39 +0300

    Add win condition logic and dynamic button label

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import Die from "./components/Die";
 function App() {
   const [dice, setDice] = useState(generateAllNewDice(10));
 
+  const gameWon =
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => die.value === dice[0].value);
+
   function generateAllNewDice(diceAmount) {
     const newDiceArray = [];
     for (let i = 0; i < diceAmount; i++) {
@@ -48,10 +52,14 @@ function App() {
 
   return (
     <main>
-      <section className="dice-container">{diceElements}</section>
-
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={rollDice}>
-        Roll
+        {gameWon ? "New Game" : "Roll"}
       </button>
     </main>
   );
